d7c841b1893450dcdae171b675830bb679322218
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2024-09-03 16:37:33 +0800

    first commit

---
+++ server/custom_modules/mymodule/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'my module ivor',
+    'version': '1.0.0',
+    'author': 'Deniel Ivor Cruz',
+    'summary': 'a simple module naiba na',
+    'description': 'A module for my odoo project',
+    'category': 'Custom',
+
+}
